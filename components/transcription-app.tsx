b37f0832dbe9b1a8d336df6fe1ffd84b14d4fffe
ake <tamiratake@gmail.com>
2024-10-17 04:35:50 +0300

    es-lint error fixed

--- components/transcription-app.tsx
+++ components/transcription-app.tsx
@@ -7,7 +7,7 @@ import { Textarea } from "@/components/ui/textarea"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Progress } from "@/components/ui/progress"
-import { AlertCircle, CheckCircle, Download, Headphones, Upload } from 'lucide-react'
+import {  CheckCircle, Download, Headphones, Upload } from 'lucide-react'
 
 export default function TranscriptionAppComponent() {
   const [transcription, setTranscription] = useState('')
